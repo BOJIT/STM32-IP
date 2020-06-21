d12c74c8d0d6d7c60bbea8c28e34c007cc9a89c0
BOJIT <35117353+BOJIT@users.noreply.github.com>
2020-06-21 16:23:24 +0100

    ptpd-lwip library now builds

--- lib/ptpd-lwip.mk
+++ lib/ptpd-lwip.mk
@@ -8,9 +8,24 @@ include src/port/port.mk
 
 ################################## SOURCES #####################################
 
-################################## TARGETS #####################################
+# Set library name (should be equivalent to the folder/submodule name)
+_LIB_NAME = ptpd-lwip
 
-default:
+_BUILD_DIR = build/lib/$(_LIB_NAME)
+_LIB_DIR = lib/$(_LIB_NAME)/src
+
+# Could be optimised? sort when rearranging later
+_SRC := arith.c bmc.c protocol.c ptpd.c dep/msg.c
+_SRC += dep/net.c dep/servo.c dep/startup.c dep/sys_time.c dep/timer.c
+
+_OBJ := $(patsubst %.c,$(_BUILD_DIR)/obj/%.o,$(_SRC))
+
+_LIB_CDEPS := -I$(_LIB_DIR) -Ilib/lwip/src/include
+# temporary - later build will have a public interface and no lwip hard-code will be needed
+
+_LIB_LDEPS := -L$(_BUILD_DIR) -l$(_LIB_NAME)
+
+################################## TARGETS #####################################
 
 # Archive into static library
 $(_BUILD_DIR)/lib$(_LIB_NAME).a: $(_OBJ)
@@ -24,7 +39,7 @@ $(_BUILD_DIR)/lib$(_LIB_NAME).a: $(_OBJ)
 	echo "$(_LIB_LDEPS)" >> build/$(PROJECT).ldep
 
 # Build all library objects
-$(_OBJ): $(_BUILD_DIR)/obj/%.o : %.c
+$(_OBJ): $(_BUILD_DIR)/obj/%.o : $(_LIB_DIR)/%.c
 	echo $(_SRC)
     # Don't link objects yet
 	mkdir -p $(@D)
